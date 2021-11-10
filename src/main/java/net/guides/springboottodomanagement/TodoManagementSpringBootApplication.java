c0891562551dc8dc0b5d5c1ac4035ecab6eef4b9
terencechan <terence.kfc@gmail.com>
2021-11-10 11:39:31 +0800

    Initial Commit

---
+++ src/main/java/net/guides/springboottodomanagement/TodoManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboottodomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/guides/springboottodomanagement/TodoManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboottodomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
